e3b5a4241d0b156e906ff5506da279549d086d1d
Chelsi Christmas <c.christmas95@gmail.com>
2020-07-28 12:25:11 -0400

    Successfuly parsed contacts dictionary

--- Playing-With-JSON.playground/Contents.swift
+++ Playing-With-JSON.playground/Contents.swift
@@ -1,3 +1,47 @@
 import UIKit
 
-var str = "Hello, playground"
+// JSON Data
+let json = """
+{
+ "results": [
+   {
+     "firstName": "John",
+     "lastName": "Appleseed"
+   },
+  {
+    "firstName": "Alex",
+    "lastName": "Paul"
+  }
+ ]
+}
+""".data(using: .utf8)!
+
+//=======================================
+// Create Model(s)
+//=======================================
+// Codable: Decodable & Encodable
+// Decodable: converts json data
+// Encodable: converts to json data to e.g POST to a Webn API
+
+// Top Level JSON is a Dictionary
+struct ResultsWrapper: Decodable {
+    let results: [Contact]
+}
+
+struct Contact: Decodable {
+    let firstName: String
+    let lastName: String
+}
+
+//=======================================
+// Create Model(s)
+//=======================================
+
+// Always go from the top level
+do{
+    let dictionary = try JSONDecoder().decode(ResultsWrapper.self, from: json)
+    let contacts = dictionary.results
+    dump(contacts)
+} catch {
+    print("decoding error: \(error)")
+}
